6c86de8ff427a4f76f023aabfa058ac1b24e829a
Max Cronce <mecronce@ucsc.edu>
2018-05-10 16:57:29 -0700

    Max: Player Drops down from ceiling now

--- Keep-It-Together-master/phaser/phaser/myGame/js/player.js
+++ Keep-It-Together-master/phaser/phaser/myGame/js/player.js
@@ -1,5 +1,5 @@
 function Player (game, key, frame) {
-	Phaser.Sprite.call(this, game, 20, 475, key, frame);
+	Phaser.Sprite.call(this, game, 400, 0, key, frame);
 	
 	this.anchor.set(0.5);
 	this.scale.setTo(2, 2);
